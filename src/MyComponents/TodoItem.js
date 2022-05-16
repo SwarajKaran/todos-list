de0fff420f0e42cd5b58a0fc8ef5a332025911a2
swarajkaran <swarajkaran.raj@gmail.com>
2022-05-16 20:01:53 +0530

    Deleting Tasks

--- src/MyComponents/TodoItem.js
+++ src/MyComponents/TodoItem.js
@@ -7,7 +7,9 @@ export const TodoItem = ({ todo: todo2, onDelete }) => {
       <p>{todo2.desc}</p>
       <button
         className="btn btn-sm btn-danger"
-        onClick={() => onDelete(todo2.sno)}
+        onClick={() => {
+          onDelete(todo2);
+        }}
       >
         Delete
       </button>
--- src/MyComponents/Todos.js
+++ src/MyComponents/Todos.js
@@ -1,15 +1,17 @@
-import React from "react";
-import { TodoItem } from "./TodoItem";
+import React from 'react';
+import { TodoItem } from './TodoItem';
 
 export const Todos = (props) => {
   return (
     <div className="container">
       <h3 className="text-center my-3">Todos List</h3>
-      {props.todos.map((todo) => {
-        return (
-          <TodoItem key={todo.sno} todo={todo} onDelete={props.onDelete} />
-        );
-      })}
+      {props.todos.length === 0
+        ? 'No Task'
+        : props.todos.map((todo) => {
+            return (
+              <TodoItem key={todo.sno} todo={todo} onDelete={props.onDelete} />
+            );
+          })}
     </div>
   );
 };
